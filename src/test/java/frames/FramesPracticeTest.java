717fe394c2d072f1b2f49e3de6eb41a665c5b4cb
Ganesh K <ganeshkannan627@gmail.com>
2023-04-02 22:35:51 +0530

    refactoring

--- src/test/java/frames/FramesPracticeTest.java
+++ src/test/java/frames/FramesPracticeTest.java
@@ -10,7 +10,7 @@ import org.testng.annotations.Test;
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 
-public class FramesPractice {
+public class FramesPracticeTest {
 
     Playwright playwright;
     Page page;
